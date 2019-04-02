95c3940607971d6db5832ea91ef71993e0b00690
emrejen <eyalmrejen@gmail.com>
2019-04-02 18:44:50 +0300

    add hidden files

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    mocha: true
+  },
+  extends: 'airbnb-base',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    indent: 2,
+    'arrow-parens': [2, 'as-needed'],
+    'no-plusplus': 0
+  },
+};
